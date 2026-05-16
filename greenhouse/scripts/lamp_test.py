4a19d4e1e4638f96be917a90f8eb4e4368fd2f1c
Derya001 <19derya89@gmail.com>
2026-05-16 18:17:47 +0200

    Remove unnecessary files

--- greenhouse/scripts/lamp_test.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO, time
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)  # LOW = kikapcs
-time.sleep(1)
-GPIO.output(17, GPIO.HIGH)  # bekapcs
-time.sleep(5)
-GPIO.output(17, GPIO.LOW)   # kikapcs
-GPIO.cleanup()
-
